f269367e46a75b06c1730771cac459b900e86fc5
Volkan Kahraman <khrmnvolkan@gmail.com>
2022-01-18 17:51:29 +0300

    error page added

---
+++ src/routes/__error.svelte
@@ -0,0 +1,27 @@
+<script context="module">
+	/** @type {import('@sveltejs/kit').ErrorLoad} */
+	export function load({ error }) {
+		return {
+			props: { error }
+		};
+	}
+</script>
+
+<script>
+	export let error;
+</script>
+
+<div class="error">
+    <h2>Ooops!</h2>
+    <p>
+        {error.message}
+    </p>
+    <a href="/games"> View All Games</a>
+</div>
+
+<style>
+    .error{
+        text-align: center;
+        margin-bottom: 60px;
+    }
+</style>
